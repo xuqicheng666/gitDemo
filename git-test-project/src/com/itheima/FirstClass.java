c653b18f6bd94e231919fc29ac53555514600ab3
tom_glb <goodMorning_glb@atguigu.com>
2020-09-24 18:18:22 +0800

    把本地工程添加到git2

---
+++ git-test-project/src/com/itheima/FirstClass.java
@@ -0,0 +1,17 @@
+package com.itheima;
+
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+        System.out.println("hello world");
+    }
+    public void test(){
+        int i=0;
+        i+=1;
+    }
+
+}
